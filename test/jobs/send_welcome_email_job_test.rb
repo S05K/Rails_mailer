0bbf3c59aed65c003ddc61fbb1da7b0ed4cccc52
Sumitraghuvanshi07 <sumit.raghuvanshi@taskcraft.in>
2023-11-30 11:11:47 +0530

    Done...

--- test/jobs/send_welcome_email_job_test.rb
+++ test/jobs/send_welcome_email_job_test.rb
@@ -4,10 +4,5 @@ class SendWelcomeEmailJobTest < ActiveJob::TestCase
   # test "the truth" do
   #   assert true
   # end
-  queue_as :default
-  def perform(user_id)
-  byebug
-    user = User.find(user_id)
-    ShippingConfirmationMailer.new_user_email(user).deliver_now
-  end
+ 
 end
